6dd8ea6d278f300d4ccf2364eed111ca2b60c3de
Emmanuel Akabike <akabikeemmanuel@gmail.com>
2025-09-08 14:29:18 +0100

    Refactored project to use Context API instead of prop drilling

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -5,7 +5,7 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 import ProfilePage from './components/ProfilePage.jsx'
-import UserContext from './UserContext.js'
+import UserContext from './components/UserContext.js'
 
 function App() {
  
