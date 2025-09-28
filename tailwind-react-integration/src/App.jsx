3449ace2f90818785983817d26b8083341f2e3c4
Emmanuel Akabike <akabikeemmanuel@gmail.com>
2025-09-28 15:07:51 +0100

    Styled UserProfile using tailwind

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,7 +1,12 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
   return ( 
+    <>
     <h1 className="text-5xl font-bold  text-blue-600">Tailwind CSS in React
     </h1>
+    <UserProfile/>
+    </>
    )}
 
 export default App;
